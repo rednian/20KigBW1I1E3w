d5844c1230819ab9a63933ae7165fea6083dc5c3
chris <rednianred@gmail.com>
2018-02-07 17:33:27 +0800

    remove error log

--- application/logs/log-2017-08-31.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2017-08-31 08:21:45 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\currv2\application\vendor/autoload.php was not found.
-ERROR - 2017-08-31 08:21:47 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\currv2\application\vendor/autoload.php was not found.
-ERROR - 2017-08-31 08:34:10 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\currv2\application\vendor/autoload.php was not found.
-ERROR - 2017-08-31 08:34:10 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\currv2\application\vendor/autoload.php was not found.
-ERROR - 2017-08-31 08:44:57 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\currv2\application\vendor/autoload.php was not found.
--- application/logs/log-2017-11-01.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2017-11-01 14:27:46 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\currv2\application\vendor/autoload.php was not found.
-ERROR - 2017-11-01 14:27:53 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\currv2\application\vendor/autoload.php was not found.
